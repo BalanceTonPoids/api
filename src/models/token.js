1e618f2fa7445c1da49c4764a809437136e8bb88
Johann six <six.johann@gmail.com>
2023-02-15 16:58:30 +0100

    🎨 add token schema

---
+++ src/models/token.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const tokenSchema = new mongoose.Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    expires: 900,// this is the expiry time in seconds
+  },
+});
+
+tokenSchema.methods.compareToken = async function (token) {
+    return token === this.token;
+};
+
+module.exports = mongoose.model("Token", tokenSchema);
