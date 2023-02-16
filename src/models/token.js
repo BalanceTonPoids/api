0b1e92188141dea58b5d5c0dae4a09e18af5b03e
Johann six <six.johann@gmail.com>
2023-02-16 12:26:11 +0100

    🎨 update lint & format

--- src/models/token.js
+++ src/models/token.js
@@ -1,29 +1,29 @@
 const mongoose = require("mongoose");
 
 const tokenSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Types.ObjectId,
-    required: true,
-    ref: "User",
-  },
-  token: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    expires: 900,// this is the expiry time in seconds
-  },
+	userId: {
+		type: mongoose.Types.ObjectId,
+		required: true,
+		ref: "User",
+	},
+	token: {
+		type: String,
+		required: true,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+		expires: 900, // this is the expiry time in seconds
+	},
 });
 
 /**
- * 
- * @param {string} token 
+ *
+ * @param {string} token
  * @returns {Promise<boolean>}
  */
 tokenSchema.methods.compareToken = async function (token) {
-    return token === this.token;
+	return token === this.token;
 };
 
 module.exports = mongoose.model("Token", tokenSchema);
--- src/utils/uid.js
+++ src/utils/uid.js
@@ -3,9 +3,9 @@
  * @returns {string} - A unique id
  */
 const uid = () => {
-    const head = Date.now().toString(36);
-    const tail = Math.random().toString(36).substring(2);
-    return head + tail;
-}
+	const head = Date.now().toString(36);
+	const tail = Math.random().toString(36).substring(2);
+	return head + tail;
+};
 
 module.exports = uid;
