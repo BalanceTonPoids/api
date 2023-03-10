8f2ef3757780ac2170acbf23043c77fd84d85a66
Johann six <six.johann@gmail.com>
2023-03-10 09:30:19 +0100

    🚚 split in two file

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -15,13 +15,5 @@ const verifyToken = (req, res, next) => {
 	return next();
 };
 
-const checkRole = (role) => (req, res, next) => {
-	if (req.user.role !== role) {
-		return res.status(401).send({"error" :"Unauthorized"});
-	}
-	next();
-};
-
 
 module.exports = verifyToken;
-module.exports = checkRole;
